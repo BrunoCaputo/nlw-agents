daa8fe0b9e180ec3457636dc98cb37114af42649
BrunoCaputo <brunopc.dev@gmail.com>
2025-07-10 23:46:35 -0300

    feat: Create questions

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,17 +1,24 @@
+import { count, eq } from 'drizzle-orm'
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { questions } from '../../db/schema/questions.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms', async () => {
     const roomsSchema = schema.rooms
+    const questionsSchema = schema.questions
 
     const results = await db
       .select({
         id: roomsSchema.id,
         name: roomsSchema.name,
+        createdAt: roomsSchema.createdAt,
+        questionsCount: count(questions.id),
       })
       .from(roomsSchema)
+      .leftJoin(questionsSchema, eq(questionsSchema.roomId, roomsSchema.id))
+      .groupBy(roomsSchema.id, roomsSchema.name)
       .orderBy(roomsSchema.createdAt)
 
     return results
