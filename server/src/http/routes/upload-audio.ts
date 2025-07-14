d2f510ba6f4d44b4bf5d33dfb80ef7d655ad0c7d
BrunoCaputo <brunopc.dev@gmail.com>
2025-07-13 21:37:27 -0300

    feat: Audio transcription with Gemini AI

--- server/src/http/routes/upload-audio.ts
+++ server/src/http/routes/upload-audio.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import z from 'zod/v4'
+import { transcribeAudio } from '../../services/gemini.ts'
 
 export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
   app.post(
@@ -11,16 +12,22 @@ export const uploadAudioRoute: FastifyPluginCallbackZod = (app) => {
         }),
       },
     },
-    async (request, reply) => {
+    async (request) => {
       const { roomId } = request.params
       const audio = await request.file()
 
-      if(!audio) {
+      if (!audio) {
         throw new Error('Audio is required')
       }
 
       // TODO
       // 1 -> Transcrever o áudio
+      const audioBuffer = await audio.toBuffer()
+      const audioAsBase64 = audioBuffer.toString('base64')
+
+      const transcription = await transcribeAudio(audioAsBase64, audio.mimetype)
+
+      return { transcription }
       // 2 -> Gerar o vetore semântico / embeddings
       // 3 -> Armazenar os vetores no banco de dados
     }
